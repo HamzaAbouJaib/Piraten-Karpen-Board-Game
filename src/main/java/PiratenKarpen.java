951403615a3ca1fe35ebaf22b924ec508e707e00
Hamza <hamza.aboujaib.0303@gmail.com>
2023-01-18 22:31:33 -0500

    Finish Step 2

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -4,6 +4,7 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
+        // run the simulation
         Simulation.runSimulation();
         System.out.println("That's all folks!");
     }
