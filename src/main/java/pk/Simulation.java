0552d0f159640a837844bd83f7dfa2c7a15efc78
Hamza <hamza.aboujaib.0303@gmail.com>
2023-01-18 22:03:06 -0500

    Create a function to simulate playing 42 games

--- src/main/java/pk/Simulation.java
+++ src/main/java/pk/Simulation.java
@@ -1,4 +1,21 @@
 package pk;
 
 public class Simulation {
+    public static void runSimulation() {
+        Player p1 = new Player(1);
+        Player p2 = new Player(2);
+        Game game = new Game();
+        for (int i = 1; i < 43; i++) {
+            p1.score = 0;
+            p2.score = 0;
+            System.out.println("Round " + i + ":");
+            game.gameRound(p1, p2);
+            if (p1.score > p2.score) {
+                System.out.println("Player 1 won!");
+            }
+            if (p2.score > p1.score) {
+                System.out.println("Player 2 won!");
+            }
+        }
+    }
 }
