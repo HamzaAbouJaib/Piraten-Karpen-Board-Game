05292d6f49021f7ae8e3ee758e394d98d9ace759
Hamza <hamza.aboujaib.0303@gmail.com>
2023-01-18 21:13:31 -0500

    Finish turn function by adding what happens if the player decides to re-roll or keep their roll and updated their score at the end.

--- src/main/java/pk/Game.java
+++ src/main/java/pk/Game.java
@@ -10,8 +10,30 @@ public class Game {
         if (skullCountChecker(dice)){
             System.out.println("Player " + p.playerID + " has rolled 3 or more skulls, their turn is over.");
         } else {
-            // Do something
+            // simulates a player deciding to re-roll or keep their roll by basically a coin flip
+            while(true) {
+                boolean reroll = Math.random() > 0.5;
+                if(reroll) {
+                    // Re-roll non skull dice and print the result of the re-roll
+                    dice = p.rerollDice(dice);
+                    System.out.print("Player " + p.playerID + "'s new roll: ");
+                    for (Faces die : dice) System.out.print(die + ", ");
+                    System.out.println();
+                    // check if the number of skulls surpassed or equals 3 after the re-roll
+                    if (skullCountChecker(dice)) {
+                        System.out.println("Player " + p.playerID + " has rolled 3 or more skulls, their turn is over.");
+                        break;
+                    }
+                } else {
+                    // simulates the player deciding to keep his roll
+                    break;
+                }
+            }
+            // Only add the score earned in the roll if the number of skulls <= 3
+            if (!skullCountChecker(dice)) p.updateScore(dice);
+            System.out.println("Player " + p.playerID + " ended their turn with a score of " + p.score);
         }
+        System.out.println("turn done");
     }
 
     public boolean skullCountChecker(Faces[] dice){
