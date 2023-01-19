232d71a7a5170722d496be39c632d605b15023a6
Hamza <hamza.aboujaib.0303@gmail.com>
2023-01-18 20:49:08 -0500

    Implement the ability to roll at the start of a turn and check if 3 or more skulls have been rolled (function to check number of skulls has not been implemented yet).

--- src/main/java/pk/Game.java
+++ src/main/java/pk/Game.java
@@ -1,5 +1,16 @@
 package pk;
 
 public class Game {
-    
+    public void turn(Player p) {
+        // At the start of a turn roll 8 dice
+        Faces[] dice= p.rollEightDice();
+        // print the faces rolled
+        for (Faces die: dice) System.out.print(die + ", ");
+        System.out.println();
+        if (skullCountChecker(dice)){
+            System.out.println("Player " + p.playerID + " has rolled 3 or more skulls, their turn is over.");
+        } else {
+            // Do something
+        }
+    }
 }
