68d84ae0d5399e84ec1467cbcf829e03aa273b65
Hamza <hamza.aboujaib.0303@gmail.com>
2023-01-23 12:26:50 -0500

    Replaced print statements with logging statements

--- src/main/java/pk/Simulation.java
+++ src/main/java/pk/Simulation.java
@@ -17,19 +17,23 @@ public class Simulation {
         for (int i = 1; i < 43; i++) {
             p1.score = 0;
             p2.score = 0;
-            System.out.println("Round " + i + ":");
+            logger.trace("Round " + i + ":");
             game.gameRound(p1, p2);
             if (p1.score > p2.score) {
-                System.out.println("Player 1 won!");
+                logger.trace("Player 1 won!");
                 playerwins[0]++;
             }
             if (p2.score > p1.score) {
-                System.out.println("Player 2 won!");
+                logger.trace("Player 2 won!");
                 playerwins[1]++;
             }
         }
-        System.out.println("42 games have been played.");
-        System.out.println("Player 1's win percentage is: " + Math.round((playerwins[0] / 42.0) * 100) + "%");
-        System.out.println("Player 2's win percentage is: " + Math.round((playerwins[1] / 42.0) * 100) + "%");
+        String p1WinPercentage = "Player 1's win percentage is: " + Math.round((playerwins[0] / 42.0) * 100) + "%";
+        String p2WinPercentage = "Player 2's win percentage is: " + Math.round((playerwins[1] / 42.0) * 100) + "%";
+        logger.trace(p1WinPercentage);
+        logger.trace(p2WinPercentage);
+        logger.trace(playerwins[0] + "    " + playerwins[1]);
+        System.out.println(p1WinPercentage);
+        System.out.println(p2WinPercentage);;
     }
 }
