3761a95e53afcb0a18b33ee780b310b959cf5ddb
Hamza <hamza.aboujaib.0303@gmail.com>
2023-01-24 17:25:15 -0500

    Finish getting user input through command line args and passing them to simulation

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -6,7 +6,7 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        // run the simulation
+        // run the simulation based on the arguments entered by the user.
         if(args.length >= 2 && args.length <= 3) Simulation.runSimulation(args[0], args[1], args.length == 3 && Objects.equals(args[2], "trace"));
         else System.out.println("Invalid number of command line arguments");
         System.out.println("That's all folks!");
